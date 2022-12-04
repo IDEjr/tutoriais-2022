5ffb88bbe9f53e31932f69b2baa12317f04191ec
ArthurEly <arthur.ely78@gmail.com>
2022-12-04 20:12:38 -0300

    adicionando todos os componentes no app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import Alinhamento from './components/alinhamento';
 import CoresGlobais from './components/coresGlobais';
 import Espacamento from './components/espacamento';
+import Box from './components/box';
+import Contador from './components/contador';
+import ExibirData from './components/exibirData';
+import ImportCSS from './components/importCSS';
+import ImportImage from './components/importImage';
 
 function App() {
   return (
